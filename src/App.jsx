1a935d118f8bce56617837172ba835b50dd44036
Devendra-1997 <devendradhakal92@gmail.com>
2024-03-17 02:28:09 +1100

    FE-Signup-feature implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import SignupPage from "./pages/authPage/signupPage";
+import VerifyEmailPage from "./pages/authPage/verifyEmailPage";
+
 function App() {
   return (
     <>
-      <h1>ecommerce app admin side</h1>
+      <Routes>
+        {/* Public Route */}
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/verify-email" element={<VerifyEmailPage />} />
+      </Routes>
+
+      <ToastContainer />
     </>
   );
 }
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import promiseReducer from "./utility/promiseSlice";
+
+const store = configureStore({
+  reducer: {
+    promise: promiseReducer,
+  },
+});
+
+export default store;
